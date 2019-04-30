5669d3ca28cef80616e8b5c1301a8122de80bd68
Gareth J. Greenaway <gareth@wiked.org>
2019-04-29 17:38:26 -0700

    The maximum for the multiprocessing queue on MacOS is 32767, so if we running on MacOS then we use that maximum.

--- tests/integration/files/log_handlers/runtests_log_handler.py
+++ tests/integration/files/log_handlers/runtests_log_handler.py
@@ -23,7 +23,8 @@ from multiprocessing import Queue
 import msgpack
 
 # Import Salt libs
-import salt.ext.six as six
+from salt.ext import six
+from salt.utils.platform import is_darwin
 import salt.log.setup
 
 log = logging.getLogger(__name__)
@@ -56,7 +57,15 @@ def setup_handlers():
             pass
         sock.close()
 
-    queue = Queue()
+    # One million log messages is more than enough to queue.
+    # Above that value, if `process_queue` can't process fast enough,
+    # start dropping. This will contain a memory leak in case `process_queue`
+    # can't process fast enough of in case it can't deliver the log records at all.
+    if is_darwin():
+        queue_size = 32767
+    else:
+        queue_size = 10000000
+    queue = Queue(queue_size)
     handler = salt.log.setup.QueueHandler(queue)
     handler.setLevel(1)
     process_queue_thread = threading.Thread(target=process_queue, args=(port, queue))
