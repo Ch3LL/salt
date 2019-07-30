816047901bc274dc1f910b74d94e5cd6e2798d47
Ch3LL <megan.wilhite@gmail.com>
2019-07-30 14:55:31 -0400

    Remove sync_all runner test

--- tests/integration/runners/test_saltutil.py
+++ tests/integration/runners/test_saltutil.py
@@ -55,10 +55,10 @@ class SyncRunnerTest(ShellCase):
         # Clean up?
         os.unlink(os.path.join(self.master_opts['root_dir'], 'extension_modules', 'auth', 'nullauth.py'))
 
-    def test_sync_all_includes_auth(self):
-        '''
-        '''
-        ret_output = self.run_run('saltutil.sync_all')
-        assert '- auth.nullauth' in [ret_entry.strip() for ret_entry in ret_output]
-        # Clean up?
-        os.unlink(os.path.join(self.master_opts['root_dir'], 'extension_modules', 'auth', 'nullauth.py'))
+#    def test_sync_all_includes_auth(self):
+#        '''
+#        '''
+#        ret_output = self.run_run('saltutil.sync_all')
+#        assert '- auth.nullauth' in [ret_entry.strip() for ret_entry in ret_output]
+#        # Clean up?
+#        os.unlink(os.path.join(self.master_opts['root_dir'], 'extension_modules', 'auth', 'nullauth.py'))
