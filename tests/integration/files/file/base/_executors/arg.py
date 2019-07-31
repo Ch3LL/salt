8e3b3336b44d68d49a0c736488b0dbc598f997ff
Ch3LL <megan.wilhite@gmail.com>
2019-07-31 12:17:52 -0400

    Add logging to arg executor

--- tests/integration/files/file/base/_executors/arg.py
+++ tests/integration/files/file/base/_executors/arg.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import logging
+log = logging.getLogger(__name__)
 
 def __virtual__():
+    log.critical('Loading arg executor')
     return True
 
 
 def execute(*args, **kwargs):
     # we use the dunder to assert the loader is provided minionmods
+    log.critical('Running arg executor')
     return __salt__['test.arg']('test.arg fired')
